6aeb58f7cc6afc520234c2fbfd057f0c15e32d44
bourguiba15p5-source <bourguiba15p5@gmail.com>
2026-04-29 17:12:59 +0200

    Add configuration for 2048 game

---
+++ config.py
@@ -0,0 +1,8 @@
+# Configuration du jeu 2048
+TAILLE = 4
+TAILLE_CASE = 100
+COULEURS = {
+    0: "#cdc1b4", 2: "#eee4da", 4: "#ede0c8", 8: "#f2b179",
+    16: "#f59563", 32: "#f67c5f", 64: "#f65e3b", 128: "#edcf72",
+    256: "#edcc61", 512: "#edc850", 1024: "#edc53f", 2048: "#edc22e"
+}
